90a411f502bc8095b0bb12f1a7f9f8907af657a1
Raj Kumar Prajapat <123717038+rajprajapat99@users.noreply.github.com>
2023-11-15 16:00:26 +0530

    new commit

--- src/BookAppointment.js
+++ src/BookAppointment.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import './App.css'
 import './App.css'
-// import ReCAPTCHA from 'react-google-recaptcha';
 import AppointForm from './AppointForm'
 const BookAppointment = () => {
 
